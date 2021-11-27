5e5d42f1b26fdc7f2955d543dd5ca0f2c211a500
Conan Bitter <conan.bitter@gmail.com>
2021-11-27 14:35:47 +0300

    Тест асинхронных роутов, добавлен блокнот с запросами

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
 import express, { Application, Request, Response } from 'express'
+import asyncHandler from 'express-async-handler';
 const app: Application = express();
 const port = 8080;
+
+async function apiTest(req: Request, res: Response): Promise<void> {
+    res.json({
+        "name": "User",
+        "items": [
+            "one",
+            "two"
+        ]
+    })
+}
 
 app.get('/', (req: Request, res: Response): void => {
     res.send('Hello World!');
 });
+
+app.get('/test', asyncHandler(apiTest));
 
 app.listen(port, (): void => {
     console.log(`Example app listening at 👉 http://localhost:${port}`);

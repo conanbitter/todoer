194c88da67bb788ba1e7cc1f4a57e6c83b432f06
Conan Bitter <conan.bitter@gmail.com>
2021-11-29 21:21:33 +0300

    Вернулся express-async-handler, упорядочены обработчики

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,16 @@
-import express, { Application, Request, Response } from 'express'
-import { asyncHandler } from './common.js';
+import express, { Application, NextFunction, Request, Response } from 'express'
+import asyncHandler from 'express-async-handler';
 const app: Application = express();
 const port = 8080;
+
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+    res.json({ "error": err.message });
+}
+
+function needLogin(req: Request, res: Response, next: NextFunction) {
+    console.log("login");
+    next();
+}
 
 async function apiTest(req: Request, res: Response): Promise<void> {
     res.json({
@@ -17,7 +26,12 @@ app.get('/', (req: Request, res: Response): void => {
     res.send('Hello World!');
 });
 
+app.use(needLogin);
+
 app.get('/test', asyncHandler(apiTest));
+
+
+app.use(errorHandler);
 
 app.listen(port, (): void => {
     console.log(`Example app listening at 👉 http://localhost:${port}`);
